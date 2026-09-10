f3fc080674cd299a98368d7df663a7d63967b4ff
Raymond Black <dragnadept@gmail.com>
2026-09-09 20:33:34 -0400

    Participation Activity 1

---
+++ Week 2/participation_activity_1.py
@@ -0,0 +1,6 @@
+participation_activity_1 = {
+	"name": "Raymond",
+	"favorite_color": "dark blue",
+	"favorite_planet": "Saturn", 
+	"other_fun_fact": "My male green cheek conure, Harley Quinn, just turned 21 years old in March.",
+	}
