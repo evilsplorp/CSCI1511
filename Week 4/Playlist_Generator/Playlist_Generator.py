8f4e39786244470272c49022c8b0cb119cffc032
Raymond Black <dragnadept@gmail.com>
2026-09-18 15:33:57 -0400

    corrected version number, removed excess comments from docstring

--- Week 4/Playlist_Generator/Playlist_Generator.py
+++ Week 4/Playlist_Generator/Playlist_Generator.py
@@ -1,16 +1,10 @@
 """
-Playlist_Generator 1.5
+Playlist_Generator 1.7
 Raymond Black
 Creates a song playlist based on user input
-Resources: used examples from the readings and from the lecture on 2026-09-09 v1.0;
-did a Google search to figure out how to push the user back to the top (continue) v1.2;
-added a limit to the playlist (mostly so I could add a new if/else) v1.2;
-found a way to add numbers to the song list when it prints v1.3;
-added notations indicating what version added a feature v1.3
-correct playlist creation limit if statement v1.4
-updated creation limit, more music if statement with a while loop v1.5
-extracted adding tracks to a function and put in separate file v1.6
-extracting yes/no to add more to a separate function file v1.7
+Resources: used examples from the readings and from the lecture;
+Google searches & 'Python for Kids' book to determine how to make 
+some minor changes;
 2026-09-18
 """
 
