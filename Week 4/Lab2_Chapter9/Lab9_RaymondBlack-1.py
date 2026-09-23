6b256b7a986fbb99e705afbf2435b64f94012357
Raymond Black <dragnadept@gmail.com>
2026-09-23 12:59:05 -0400

    added coin toss, get results, and printing results (but not winner/loser)

--- Week 4/Lab2_Chapter9/Lab9_RaymondBlack-1.py
+++ Week 4/Lab2_Chapter9/Lab9_RaymondBlack-1.py
@@ -11,9 +11,11 @@ from player import Player
 
 
 def main():
+    """
+    Get some player names
+    """
     player1 = Player(input("Enter Player 1 name: "))
     player2 = Player(input("Enter Player 2 name: "))
-    # return player1, player2
 
     play = input("\nDo you want to toss a coin? (y/n))").strip().lower()
 
@@ -21,18 +23,33 @@ def main():
         print("Let\'s toss those coins!")
 
 # toss coin for each player
+    player1.toss_coin()
+    player2.toss_coin()
 
 # get result for each coin toss
+    side1 = player1.get_coin_side()
+    side2 = player2.get_coin_side()
 
 # print name and who tossed what
+    print(f"{player1} got {side1}\n")
+    print(f"{player2} got {side2}")
 
-# print winner
+# print winners
+# If the sides match: Call the appropriate methods for the 
+# winner to win_coin() and the loser to lose_coin(). 
+# Print who won the round.
+# If the sides do not match: Call the appropriate methods for 
+# Player 2 to win_coin() and Player 1 to lose_coin(). 
+# Print who won the round.
 
 # update wallets
+# Report the total coins for each player using get_wallet().
+
 
 # if either player has an empty wallet, end the game
 # otherwise, ask if they want to play again
 
-# empty wallet or end game, provide winner info
+# if empty wallet or end game selected, provide winner info
 
-# call the function?
+# call the function to start
+main()
