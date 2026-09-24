c1f7f4dae7f1b8bdd6e3751e8b6d1bfe10b5d8ca
Raymond Black <dragnadept@gmail.com>
2026-09-24 11:46:55 -0400

    added new function for name input validation

--- Week 4/Lab2_Chapter9/player.py
+++ Week 4/Lab2_Chapter9/player.py
@@ -6,55 +6,58 @@ class Player:
     Raymond Black
     A simple coin matching game. 2 players start with 20 points.
     If the coin is heads, one player gains a coin, the other loses
-    a coin (and vice versa).
+    a coin (and vice versa). 
     This class handles the player functionality.
     2026-09-22
     """
     def __init__(self, name):
-        """set default sides to Heads"""
+        """Sets a name argument"""
         self.__name = name
         self.__wallet = 20
         self.__coin = Coin()
 
-    @property
-    def name(self):
-        """Allows main.py to use 'player.name' instead of 'player.get_name()'"""
-        return self.__name    
-
     def toss_coin(self):
         """
-        Tells the player's coin to toss itself.
+        Toss the player's coin
         """
         self.__coin.toss()
 
     def get_coin_side(self) -> str:
         """
-        Gets the side of the player's coin and returns its value.
+        Gets the player's coin and returns the value
         """
         return self.__coin.get_sideup()
 
     def win_coin(self):
         """
-        Adds 1 to the wallet.
+        Add 1 to the wallet when they win
         """
         self.__wallet += 1
 
     def lose_coin(self):
         """
-        Subtracts 1 from the wallet.
+        Subtract 1 from the wallet when they lose
         """
         self.__wallet -= 1
 
     def get_wallet(self) -> int:
         """
-        Returns the current value of the wallet.
+        Shows the value of the wallet
         """
         return self.__wallet
 
     def get_name(self) -> str:
         """
-        Returns the value of the player's name.
+        Returns the player's name
         """
         return self.__name
 
-
+    def is_valid_name(self, other: Player | None = None):
+        """
+        Returns True if valid, or a reason why it was invalid otherwise.
+        """
+        if self.get_name() == "":
+            return "Nothing was entered."
+        if other != None and self.get_name() == other.get_name():
+            return "Sorry, that name was already taken. "
+        return True
