df07f6398973516739542aceacbf4a8f6a5e0ce8
Raymond Black <dragnadept@gmail.com>
2026-09-24 11:48:01 -0400

    modified formattting, added new code to use name validation function

--- Week 4/Lab2_Chapter9/Lab9_RaymondBlack-1.py
+++ Week 4/Lab2_Chapter9/Lab9_RaymondBlack-1.py
@@ -10,7 +10,6 @@ This is the application itself.
  """
 from player import Player
 
-
 def main():
     """
     Get player names for coin game. Toss coins, compare coins, determine
@@ -29,24 +28,18 @@ def main():
         elif play == "y":
 
             player1 = Player(input("We need names. First player? "))
-            print(f"{player1.get_name()}")
-            while player1.get_name() == "":
-                player1 = Player(input("Please enter a name, Player 1: "))
-                if player1.get_name() == "":
-                    continue
-                else:
-                    break
+            valid_or_reason = player1.is_valid_name()
+
+            while valid_or_reason != True:
+                player1 = Player(input(f"{valid_or_reason} Please enter a name, Player 1: "))
+                valid_or_reason = player1.is_valid_name()
 
             player2 = Player(input("Second player? "))
-            while player2.get_name() == "":
-                player2 = Player(input("Please enter a name, Player 2: "))
-                if player2.get_name() == "":
-                    continue
-                elif player2.get_name() == player1.get_name():
-                    player2 = Player(input("Sorry, that name is taken. Choose another? "))
-                    continue
-                else:
-                    break
+            valid_or_reason = player2.is_valid_name(player1)
+
+            while valid_or_reason != True:
+                player2 = Player(input(f"{valid_or_reason} Please enter a name, Player 2: "))
+                valid_or_reason = player2.is_valid_name(player1)
 
             print(f"\nWelcome {player1.get_name()} and {player2.get_name()}! You both start with 20 coints.")
             while play == "y":
@@ -83,26 +76,26 @@ def main():
                     print(f"{player2.get_name()} is out of coins! {player1.get_name()} wins!")
                     break
 
-                play = input("\nDo you want to toss another coin? (y/n))\n").strip().lower()
+                play = input("\nDo you want to toss another coin? (y/n)) ").strip().lower()
                 if play == "y":
                     continue
                 elif play == "n":
                     break
                 else:
                     print("Please enter y or n.")
-                    play = input("\nDo you want to toss another coin? (y/n))\n").strip().lower()
+                    play = input("\nDo you want to toss another coin? (y/n)) ").strip().lower()
                     continue
 
-            print("-=-=-=-=-=-=-=GAME OVER=-=-=-=-=-=-=-\n")
+            print("\n-=-=-=-=-=-=-=GAME OVER=-=-=-=-=-=-=-\n")
 
             wallet1 = player1.get_wallet()
             wallet2 = player2.get_wallet()
 
             if wallet1 > wallet2:
-                print(f"{player1.get_name()} wins!\n")
+                print(f"{player1.get_name()} wins! 🏆\n")
                 print(f"The score is {wallet1} coins to {wallet2} coins.\n")
             elif wallet1 < wallet2:
-                print(f"{player2.get_name()} wins!\n")
+                print(f"{player2.get_name()} wins! 🏆\n")
                 print(f"The score is {wallet2} coins to {wallet1} coins.\n")
             else:
                 print("It's a tie!\n")
